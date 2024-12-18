8be14d1ef06715eee6519470f04aa448e91fcc67
Shoaib Mohammed <smohammed7@hawk.iit.edu>
2024-12-17 21:20:34 -0600

    files

--- hooks/prisma/migrations/20240713145837_init/migration.sql
+++ hooks/prisma/migrations/20240713145837_init/migration.sql
@@ -12,7 +12,6 @@ CREATE TABLE "User" (
 CREATE TABLE "Zap" (
     "id" TEXT NOT NULL,
     "triggerId" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Zap_pkey" PRIMARY KEY ("id")
 );
@@ -22,7 +21,6 @@ CREATE TABLE "Trigger" (
     "id" TEXT NOT NULL,
     "zapId" TEXT NOT NULL,
     "triggerId" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL DEFAULT '{}',
 
     CONSTRAINT "Trigger_pkey" PRIMARY KEY ("id")
 );
@@ -32,8 +30,6 @@ CREATE TABLE "Action" (
     "id" TEXT NOT NULL,
     "zapId" TEXT NOT NULL,
     "actionId" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL DEFAULT '{}',
-    "sortingOrder" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
 );
@@ -42,7 +38,6 @@ CREATE TABLE "Action" (
 CREATE TABLE "AvailableAction" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
 
     CONSTRAINT "AvailableAction_pkey" PRIMARY KEY ("id")
 );
@@ -51,36 +46,12 @@ CREATE TABLE "AvailableAction" (
 CREATE TABLE "AvailableTrigger" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
 
     CONSTRAINT "AvailableTrigger_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "ZapRun" (
-    "id" TEXT NOT NULL,
-    "zapId" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL,
-
-    CONSTRAINT "ZapRun_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ZapRunOutbox" (
-    "id" TEXT NOT NULL,
-    "zapRunId" TEXT NOT NULL,
-
-    CONSTRAINT "ZapRunOutbox_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Trigger_zapId_key" ON "Trigger"("zapId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "ZapRunOutbox_zapRunId_key" ON "ZapRunOutbox"("zapRunId");
-
--- AddForeignKey
-ALTER TABLE "Zap" ADD CONSTRAINT "Zap_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_triggerId_fkey" FOREIGN KEY ("triggerId") REFERENCES "AvailableTrigger"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -93,9 +64,3 @@ ALTER TABLE "Action" ADD CONSTRAINT "Action_zapId_fkey" FOREIGN KEY ("zapId") RE
 
 -- AddForeignKey
 ALTER TABLE "Action" ADD CONSTRAINT "Action_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "AvailableAction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ZapRun" ADD CONSTRAINT "ZapRun_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ZapRunOutbox" ADD CONSTRAINT "ZapRunOutbox_zapRunId_fkey" FOREIGN KEY ("zapRunId") REFERENCES "ZapRun"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
